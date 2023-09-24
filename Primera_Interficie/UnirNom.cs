9e52d9125cfcbf795ca6e89b5026469ffef87bd2
Julia Fernandez <jferna6@institutcampalans.net>
2023-09-24 20:57:34 +0200

    Primer commit

---
+++ Primera_Interficie/UnirNom.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Primera_Interficie
+{
+    public class UnirNom : INotifyPropertyChanged
+    {
+        private string nom, cognom, nom_complet;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void OnPropertyChanged(string property)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(property));
+            }
+        }
+
+        public string Nom 
+        { 
+            get { return nom; }
+
+            set { nom = value;
+
+                OnPropertyChanged("Nom_Complet");
+            
+            }
+        }
+
+
+        public string Cognom
+        {
+            get { return cognom; }
+
+            set { cognom = value;
+
+                OnPropertyChanged("Nom_Complet");
+
+            }
+        }
+
+
+        public string Nom_Complet
+        {
+            get { nom_complet = Nom + " " + Cognom;
+
+                return nom_complet; 
+            }
+
+            set { cognom = value; }
+        }
+
+    }
+}
